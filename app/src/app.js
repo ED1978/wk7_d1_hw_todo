ef0a97e4cd71db697000edcdcb3d1aa6170f766a
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-07 21:03:57 +0100

    radio buttons working

--- app/src/app.js
+++ app/src/app.js
@@ -6,17 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
     el: "#app",
     data: {
       toDos:[
-        {task: "Laundry"},
-        {task: "Dishes"},
-        {task: "Shopping"}
+        {task: "Laundry", priority: "low"},
+        {task: "Dishes", priority: "high"},
+        {task: "Shopping", priority: "high"}
       ],
       newToDo: ""
     },
     methods: {
       saveNewToDo: function() {
         this.toDos.push({
-          task: this.newToDo
-        })
+          task: this.newToDo,
+          priority: this.newPriority
+        });
+        this.newToDo = "";
       }
     }
   })
